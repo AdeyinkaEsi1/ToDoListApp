8f288f484a471028440fd9a4e1f70573a2a70afa
AdeyinkaEsi1 <yhamid2828@gmail.com>
2023-05-07 09:15:39 +0100

    added * to 'Allowed hosts'

--- global_file/settings.py
+++ global_file/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1fts%ov=zn7nua0&d_=ir&=h*_d(a1&k=o93(_rx7us5d9c41o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
