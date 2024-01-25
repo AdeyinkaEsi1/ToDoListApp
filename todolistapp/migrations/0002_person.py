f234b586d97e050b3a2d8473b9da94f38af3246c
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-01-25 17:46:47 +0100

    new commit

--- todolistapp/migrations/0002_person.py
+++ todolistapp/migrations/0002_person.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('basefile', '0001_initial'),
+        ('todolistapp', '0001_initial'),
     ]
 
     operations = [
