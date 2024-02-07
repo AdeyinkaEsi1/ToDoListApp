4b0496d3d599b822d4b95890cf53a6d7932b7dd3
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-02-07 20:16:28 +0100

    added logout route

--- todolistapp/urls.py
+++ todolistapp/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
-from .views import Tasklist, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, RegisterPage
+from .views import Tasklist, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, RegisterPage, CustomLogoutView
 from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
    path('register/', RegisterPage.as_view(), name='register'),
    path('login/', CustomLoginView.as_view(), name='login'),
-   path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+   # path('logout', LogoutView.as_view(next_page='login'), name='logout'),
+   path('logout/', CustomLogoutView.as_view(), name='logout'),
    path('', Tasklist.as_view(), name='tasks'),
    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
    path('task-create/', TaskCreate.as_view(), name='task-create'),
    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
-   # path('people/', PersonListView.as_view(), name='birthdays'),
 ]
