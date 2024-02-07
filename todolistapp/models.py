f918bad1e3890e7e7daeec5b64e8b02fc00d0e4d
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-02-07 20:14:58 +0100

    resolved no input task submission leading to type __str__ error

--- todolistapp/models.py
+++ todolistapp/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-#
 
 
 class Task(models.Model):
@@ -10,20 +9,10 @@ class Task(models.Model):
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
 
+
     def __str__(self):
-        return self.title
+        return f"{self.title}" if self.title else "Task with no title"
+    
 
     class Meta:
         ordering = ['complete']
-
-
-
-# class Person(models.Model):
-#     name = models.CharField(max_length=100)
-#     birth_date = models.DateField()
-
-#     def __str__(self):
-#         return self.name
-
-#
-
