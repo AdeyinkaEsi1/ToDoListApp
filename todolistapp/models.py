0c8d5ac58afb27430dcd5d130cf40b861cc27038
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-02-07 20:32:18 +0100

    modified title and desc to required(blank=false)

--- todolistapp/models.py
+++ todolistapp/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=200, null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
+    title = models.CharField(max_length=200, null=True, blank=False)
+    description = models.TextField(null=True, blank=False)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
 
